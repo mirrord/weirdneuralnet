- added reference test for full network prediction and backprop - fixed traversal bug that spontaneously created connections

main.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from network import WeirdNetwork
+import node_utils
 
 import os
 import hashlib, requests, gzip
@@ -107,8 +108,23 @@ def load_test(fname):
     print(f"cost: {final_error}")
     return model
 
+def equivalence_test(fname):
+    X_train, Y_train, X_test, Y_test, X_val, Y_val = get_dataset('./datasets/')
+    model = WeirdNetwork.load(fname)
+    weights, biases = model.compile()
+    model_prediction = model.predict(X_train)
+    classic_prediction = node_utils.classic_net_predict(weights, biases, X_train)
+    print('####################################')
+    assert(np.array_equal(model_prediction, classic_prediction))
+    #print(f"model output ({model_prediction.shape}): {model_prediction}")
+    #print(f"classic output ({classic_prediction.shape}): {classic_prediction}")
+
+    model_error = model.backpropagate(X_train, Y_train)
+    classic_error = node_utils.classic_net_backprop(weights, biases, X_train, Y_train)
+    print(f"model error: {[(i, m.shape) for i,m in model_error[0].items()]}, {[(i,m.shape) for i,m in model_error[1].items()]}")
+    print(f"class error: {[m.shape for m in classic_error[0]]}, {[m.shape for m in classic_error[1]]}")
 
 if __name__=="__main__":
-    model = run_test(10)
+    #model = run_test(100)
     load_model = load_test("my_model.wn")
-    
+    #equivalence_test("my_model.wn")

network.py
@@ -22,12 +22,14 @@ class WeirdNetwork():
         for idx, param in enumerate(node_params):
             self.feed_indices[idx] = [edge[1] for edge in edges if edge[0]==idx]
             self.backfeed_indices[idx] = [edge[0] for edge in edges if edge[1]==idx]
+            #print(f"backfeed idxs {idx}: {self.backfeed_indices[idx]}")
             self.nodes.append(Node(param['x'], param['y'], param['activation']))
             if param.get('output', False):
                 self.output_node = idx
             if param.get('input', False):
                 self.input_indices.append(idx)
         self.edges = edges
+
 
     def __str__(self):
         return f"<WeirdNetwork size={len(self.nodes)} nodes>"
@@ -47,9 +49,25 @@ class WeirdNetwork():
             p = Pickler(f)
             return p.dump(self)
 
+    def compile(self):
+        # only works for linear networks
+        to_traverse = []
+        to_traverse.extend(self.input_indices)
+        for idx in self.input_indices:
+            to_traverse.extend(self.feed_indices[idx])
+        for idx in to_traverse:
+            to_traverse.extend([fidx for fidx in self.feed_indices[idx] if fidx not in to_traverse])
+        if self.output_node in to_traverse:
+            weights = [self.nodes[nidx].weight for nidx in to_traverse]
+            biases = [self.nodes[nidx].bias for nidx in to_traverse]
+            return weights, biases
+        raise Exception("Output node is not fed")
+
     def predict(self, input):
         outputs = {}
         to_traverse = []
+        #print(f"feed idxs: {self.feed_indices}")
+        #print(f"backfeed idxs: {self.backfeed_indices}")
         #print(f"predicting... {input.shape}")
         for idx in self.input_indices:
             #print(f"feeding input node {idx}...")
@@ -79,12 +97,15 @@ class WeirdNetwork():
     def backpropagate(self, input, exp_output):
         num_sample = input.shape[1]
         predicted_output = self.predict(input)
-        #print(f"output: {predicted_output.shape}")
+        # print(f"output: {predicted_output.shape}")
         backfeed = {}
         error_delta = self.cost_deriv(predicted_output, exp_output)
-        #print(f"dE {error_delta.shape} = dC( {predicted_output.shape}, {exp_output.shape} )")
+        # print(f"dE {error_delta.shape} = dC( {predicted_output.shape}, {exp_output.shape} )")
+        # print(f"(backprop) before backfeed: {self.backfeed_indices}")
         backfeed[self.output_node] = self.nodes[self.output_node].backfeed(error_delta)
-        to_traverse = self.backfeed_indices.get(self.output_node, [])
+        # print(f"(backprop) after backfeed: {self.backfeed_indices}")
+        to_traverse = self.backfeed_indices.get(self.output_node, []).copy()
+        # print(f"(backprop) after to_traverse: {self.backfeed_indices}")
         for idx in to_traverse:
             error_signal_components = [backfeed.get(oidx, (0,0,0)) for oidx in self.feed_indices[idx]]
             de = sum([i[2] for i in error_signal_components]) #I think this actually requires a derivative of the synapse func
@@ -95,16 +116,13 @@ class WeirdNetwork():
                 backfeed[idx][0]+=db
                 backfeed[idx][1]+=dw
                 backfeed[idx][2]+=de
+
             for jdx in self.backfeed_indices[idx]:
                 if jdx not in to_traverse:
                     to_traverse.append(jdx)
-        #take average of gradients
+
         bup = {i:np.sum(b[0],1,keepdims=True)/num_sample for i,b in backfeed.items()}
         wup = {i:w[1]/num_sample for i,w in backfeed.items()}
-        # print([b.shape for b,_,_ in backfeed.values()])
-        # print([w.shape for _,w,_ in backfeed.values()])
-        # print([b.shape for b in bup.values()])
-        # print([w.shape for w in wup.values()])
         return bup, wup
 
     def evaluate(self, input, exp_out):

node.py
@@ -26,7 +26,10 @@ class Node():
         self.input = input
         self.z = np.dot(self.weight, input) + self.bias
         self.output = self.activate(self.z)
-        #print(f"δ({self.weight.shape} . {input.shape} + {self.bias.shape}) = {self.output.shape}")
+        # print(f"δ({self.weight.shape} . {input.shape} + {self.bias.shape}) = {self.output.shape}")
+        # print(f"weight: {self.weight}\n")
+        # print(f"input: {input}\n")
+        # print(f"output: {self.output}\n")
         return self.output
 
     def backfeed(self, de_dz_foward):