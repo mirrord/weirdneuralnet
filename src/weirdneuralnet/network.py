* impl dscale, norm & dnorm, l2norm & dl2norm activations  * fixed DualInputNode->DualNueralNode  * fixed input site-based activation/backprop  * fixed DelayNode (I think, still needs testing)  * impl more experiment stuff  * added experiment result graphs

src/weirdneuralnet/network.py
@@ -1,6 +1,6 @@
 
 
-from .node import DelayNode, DualNeuralNode, NeuralNode
+from .node import DelayNode, DualInputNode, NeuralNode
 from .node_utils import *
 from .cluster import *
 #import numpy as np
@@ -179,7 +179,7 @@ class WeirdNetwork():
             self.feed_indices[idx] = [(edge[1], edge[2]) for edge in self.edges if edge[0]==idx]
             for outp, inp, site in self.edges:
                 if inp == idx:
-                    self.backfeed_indices[(idx,site)] = [edge[0] for edge in self.edges if edge[1]==idx]
+                    self.backfeed_indices[(idx,site)] = [edge[0] for edge in self.edges if edge[1]==idx and edge[2]==site]
             node_type = param.get("type", "neural")
             if node_type == "neural":
                 self.nodes.append(NeuralNode(
@@ -187,8 +187,8 @@ class WeirdNetwork():
                     param.get('activation', ''),
                     param.get('normalize', '')
                 ))
-            elif node_type == "self-attention":
-                self.nodes.append(DualNeuralNode(
+            elif node_type == "dual-input":
+                self.nodes.append(DualInputNode(
                     param.get('activation', 'scaled'),
                     param.get('normalize', '')
                 ))
@@ -302,6 +302,7 @@ class WeirdNetwork():
         fired = []
         to_traverse = []
         for idx in self.input_indices:
+            #print(f"feeding input node {idx}")
             self.nodes[idx].feed({0:[input]})
             fired.append(idx)
             to_traverse.extend(self.feed_indices[idx])
@@ -326,13 +327,13 @@ class WeirdNetwork():
         '''Calculate weight & bias updates using gradient descent.
         Inputs
             input
-                vector on which to calculate error singal, shape (features, samples).
+                vector on which to calculate error signal, shape (features, samples).
             exp_output
                 vector to compare with model prediction, shape (binarized classes, samples).
         '''
         num_sample = exp_output.shape[0]
         predicted_output = self.nodes[self.output_node].output
-        backfeed = {}
+        backfeed = {}# node index: (db, dw, [ error signals by input site ])
         error_delta = self.cost_deriv(predicted_output, exp_output)
         backfeed[self.output_node] = self.nodes[self.output_node].backfeed(error_delta)
         to_traverse = []
@@ -340,23 +341,18 @@ class WeirdNetwork():
             to_traverse.extend(self.backfeed_indices.get(((self.output_node,site)), []).copy())
 
         for idx in to_traverse:
-            error_signal_components = [backfeed.get(oidx, (0,0,0)) for oidx,_ in self.feed_indices[idx]]
-            de = sum([i[2] for i in error_signal_components]) #I think this actually requires a derivative of the synapse func
+            error_signal_components = [backfeed.get(oidx, (0,0,[0]*(site+1)))[2][site] for oidx,site in self.feed_indices[idx]]
+            de = sum([i for i in error_signal_components])
             db, dw, de = self.nodes[idx].backfeed(de)
-            if idx not in backfeed:
-                backfeed[idx] = db, dw, de
-            else:
-                backfeed[idx][0]+=db
-                backfeed[idx][1]+=dw
-                backfeed[idx][2]+=de
+            backfeed[idx] = db, dw, de
 
             for site in range(self.nodes[idx].num_input_sites):
                 for jdx in self.backfeed_indices.get((idx,site), []):
                     if jdx not in to_traverse:
                         to_traverse.append(jdx)
 
-        bup = {i:np.sum(b[0],0,keepdims=True)/num_sample for i,b in backfeed.items()}
-        wup = {i:w[1]/num_sample for i,w in backfeed.items()}
+        bup = {i:np.sum(b[0],0,keepdims=True)/num_sample for i,b in backfeed.items() if self.nodes[i].does_update()}
+        wup = {i:w[1]/num_sample for i,w in backfeed.items() if self.nodes[i].does_update()}
         return bup, wup
 
     def train(self,

src/weirdneuralnet/node.py
@@ -30,6 +30,9 @@ class BaseNode():
         '''Retrieve the last-calculated output of this node, or 0 if never activated.'''
         return self.output
 
+    def does_update(self):
+        return True
+
     #virtual
     def _fire(self):
         '''calculates feed-foward output'''
@@ -50,7 +53,7 @@ class BaseNode():
 
     #virtual
     def backfeed(self, de_dz_foward:np.array):
-        return 0, 0, de_dz_foward
+        return 0, 0, [de_dz_foward]
 
     #virtual
     def update(self, delta_bias:np.array, delta_weight:np.array):
@@ -113,14 +116,14 @@ class NeuralNode(BaseNode):
         '''
         delta_bias = de_dz_foward * self.backtivate(self.z)
         delta_weight = np.dot(self.inputs[0].T, delta_bias)
-        return delta_bias, delta_weight, np.dot(delta_bias, self.weight.T) #last is de_dz for next layer down
+        return delta_bias, delta_weight, [np.dot(delta_bias, self.weight.T)] #last is de_dz for next layer down
 
     def update(self, delta_bias:np.array, delta_weight:np.array):
         '''Update the weights and biases by subtraction.'''
         self.bias -= delta_bias
         self.weight -= delta_weight
 
-class DualNeuralNode(BaseNode):
+class DualInputNode(BaseNode):
     def __init__(self,
                 activation:str,
                 normalize:str=''):
@@ -129,10 +132,8 @@ class DualNeuralNode(BaseNode):
             activation - string name of activation function to use
             normalize - string name of normalization function to use
         '''
-        #TODO: impl initialization strats
-        # currently using Xavier
         super().__init__(2)
-        self.activate, _ = ACTIVATIONS.get(activation, (no_activation, no_activation))
+        self.activate, self.backtivate = ACTIVATIONS.get(activation, (no_activation, no_activation))
         self.normalize_label, self.normalize = normalize, NORMALIZATIONS.get(normalize, nonorm)
         self.activation_label = activation
         self._shape = None
@@ -141,6 +142,8 @@ class DualNeuralNode(BaseNode):
         self._shape = dims
     def shape(self):
         return self._shape
+    def does_update(self):
+        return False
 
     def reload(self):
         '''Re-establish function pointers from stored function names.'''
@@ -156,12 +159,12 @@ class DualNeuralNode(BaseNode):
     def _fire(self):
         '''Calculate the foward activation with some input.
         Inputs:
-            input - the input matrix, which must be of shape (num features, num samples)
+            input - the input matrices, which must be of shape (num features, num samples)
         '''
         if self.normalize_label:
             self.inputs[0] = self.normalize(self.inputs[0])
             self.inputs[1] = self.normalize(self.inputs[1])
-        self.z = np.dot(self.inputs[0], self.inputs[1].T)
+        self.z = self.inputs[0] * self.inputs[1]
         self.output = self.activate(self.z)
         return self.output
 
@@ -170,8 +173,12 @@ class DualNeuralNode(BaseNode):
         Inputs:
             de_dz_forward - the next layers' error signal
         '''
-        #TODO: I'm not certain how this should be calculated, if at all
-        return 0, 0, de_dz_foward #last is de_dz for next layer down
+        db = de_dz_foward * self.backtivate(self.z)
+        de_dz = [
+            db * self.inputs[1],
+            db * self.inputs[0],
+        ]
+        return 0, 0, de_dz
 
 class DelayNode(BaseNode):
     def __init__(self, dims=None):
@@ -188,6 +195,8 @@ class DelayNode(BaseNode):
     def set_dims(self, dims):
         if self.output == None:
             self.output = np.zeros(dims)
+    def does_update(self):
+        return False
 
     def clear_history(self):
         super().clear_history()
@@ -199,4 +208,7 @@ class DelayNode(BaseNode):
             self.output = np.zeros(self.inputs[0].shape)
         self.next_output = self.inputs[0]
         return self.output
+
+    def backfeed(self, de_dz_foward:np.array):
+        return 0, 0, [0]
         

src/weirdneuralnet/node_utils.py
@@ -53,11 +53,24 @@ def dswish(x):
     return swishx+(sigx*(1-swishx))
 
 def scale_act(x):
-    return x/len(x)
+    return x/x.shape[1]
+def dscale(x):
+    return 1/x.shape[1]
+
+def norm(x):
+    return x/np.max(x,axis=1,keepdims=True)
+def dnorm(x):
+    return 1/np.max(x,axis=1,keepdims=True)
+
+def l2norm(x):
+    return x/np.linalg.norm(x,axis=1,keepdims=True)
+def dl2norm(x):
+    return 1/np.linalg.norm(x,axis=1,keepdims=True)
 
 def no_activation(x):
     return x
 
+#TODO: wrap these and log a warning when label is not found
 ACTIVATIONS ={
     "sigmoid": (sigmoid, dsigmoid),
     "relu": (relu, drelu),
@@ -65,7 +78,9 @@ ACTIVATIONS ={
     "tanh": (tanh, dtanh),
     "softmax": (softmax, dsoftmax),
     "swish": (swish, dswish),
-    "scaled": (scale_act, no_activation)
+    "scale": (scale_act, dscale),
+    "norm": (norm, dnorm),
+    "l2norm": (l2norm, dl2norm),
 }
 
 ## cost functions